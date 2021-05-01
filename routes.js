faffaf4a844af38d8e5aa3f64a7a17a67def3485
jnajera06 <jose.najera06@gmail.com>
2021-05-01 23:07:44 +0000

    mongoDB and MVC

--- routes.js
+++ routes.js
@@ -1,15 +1,19 @@
 const express = require('express'),
 router = express.Router();
 
-router.get('/hello/:foo/:bar', (req, res) => {
-    res.json({message: 'Hello BScBest!', data: [
-        req.params.foo,
-        req.params.bar
-    ]});
-});
+var itemCtrl = require('./item-controller'),
+userCtrl = require('./user-controller');
 
-router.post('/hello', (req, res) => {
-    res.json({result: 'Post was sent', data: req.body});
-});
+router.get('/hello', itemCtrl.getWorld);
+
+router.get('/hello/:foo/:bar', itemCtrl.getWorldParams);
+
+router.post('/hello', itemCtrl.postWorld);
+
+router.post('/users', userCtrl.createUser);
+router.get('/users', userCtrl.getUsers);
+router.get('/users/:id', userCtrl.getUser);
+router.put('/users/:id', userCtrl.updateUser);
+router.delete('/users/:id', userCtrl.deleteUser);
 
 module.exports = router;
